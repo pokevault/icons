447b760a9d555d38a0a9634a57d5f8fd8205255e
libdev-hacker <libdev-hacker@tuta.io>
2024-11-12 21:16:29 +0000

    Initial Commit
    - Preparing branch to be compatible with PokeVault

---
+++ scripts/regular.py
@@ -0,0 +1,33 @@
+import os
+import json
+import re
+
+files = os.scandir("../pokemon/regular")
+female_files = os.scandir("../pokemon/regular/female")
+dex = json.load(open("pokedex/pokedex.json", "r"))
+
+for file in files:
+    name = file.name
+    for entry in dex:
+        pattern = r"^{}(?=\.|-)".format(entry["species"])
+        match = re.match(pattern, name)
+        if match:
+            print("new name: ", name.replace(entry["species"], str(entry["pokedex_number"])))
+            os.rename(file.path, file.path.replace(entry["species"], str(entry["pokedex_number"])))
+            break
+        else:
+            continue
+    continue
+
+for file in female_files:
+    name = file.name
+    for entry in dex:
+        pattern = r"^{}(?=\.|-)".format(entry["species"])
+        match = re.match(pattern, name)
+        if match:
+            print("new name: ", name.replace(entry["species"], str(entry["pokedex_number"])))
+            os.rename(file.path, file.path.replace(entry["species"], str(entry["pokedex_number"])))
+            break
+        else:
+            continue
+    continue
